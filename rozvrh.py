c75d5cdd61d62de9fc2c453d63d00713ce390373
Richard Žáček <zacek.richard@purkynka.cz>
2025-10-13 10:51:19 +0200

    Add files via upload

--- rozvrh.py
+++ rozvrh.py
@@ -104,9 +104,6 @@ def get_rozvrh():
     rozvrh_obj = vytvor_rozvrh_objekt(rozvrh, ucebny, ucitele)
     return jsonify(rozvrh_obj)
 
-
 if __name__ == '__main__':
-    print("\n" + "!"*50)
-    print("Rozvrh http://localhost:5000")
-    print("!"*50 + "\n")
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
